c2237b71289f97df05d11e24e47af16cf3007feb
Sergey <sp@outsi.de>
2022-01-23 22:58:51 +0300

    fixes

--- src/js/numbers.js
+++ src/js/numbers.js
@@ -1,27 +1,27 @@
-$(window).scroll(testScroll);
-let viewed = false;
+// $(window).scroll(testScroll);
+// let viewed = false;
 
-function testScroll() {
-  if (isScrolledIntoView($('.numbers__number')) && !viewed) {
-    viewed = true;
-    $('.numbers__number span').each(function() {
-      let w = $(this).width();
-      $(this).css('display', 'block');
-      $(this).css('width', w + 10);
-      $(this)
-        .prop('Counter', 0)
-        .animate(
-          {
-            Counter: $(this).text(),
-          },
-          {
-            duration: 2000,
-            easing: 'swing',
-            step: function(now) {
-              $(this).text(Math.ceil(now));
-            },
-          },
-        );
-    });
-  }
-}
+// function testScroll() {
+//   if (isScrolledIntoView($('.numbers__number')) && !viewed) {
+//     viewed = true;
+//     $('.numbers__number span').each(function() {
+//       let w = $(this).width();
+//       $(this).css('display', 'block');
+//       $(this).css('width', w + 10);
+//       $(this)
+//         .prop('Counter', 0)
+//         .animate(
+//           {
+//             Counter: $(this).text(),
+//           },
+//           {
+//             duration: 2000,
+//             easing: 'swing',
+//             step: function(now) {
+//               $(this).text(Math.ceil(now));
+//             },
+//           },
+//         );
+//     });
+//   }
+// }
